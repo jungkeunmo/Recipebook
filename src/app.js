1a0b1cb8d7cd1918e3fe2f79c965f2429c986b35
“jungkeunmo“ <abc59684495@gmail.com>
2021-03-11 21:42:18 +0900

    d

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ dotenv.config();
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import connect from "../db/mongo";
+import path from "path";
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(morgan(`dev`));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "/assets")));
 connect();
 
 app.use("/", globalRouter);
