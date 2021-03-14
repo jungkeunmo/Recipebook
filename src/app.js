a4c545e27a6b3c977424ec6fc94a6faa44e5feb4
“jungkeunmo“ <abc59684495@gmail.com>
2021-03-14 13:02:18 +0900

    home

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
-import connect from "../db/mongo";
 import path from "path";
 
 const PORT = process.env.PORT;
@@ -12,9 +10,10 @@ const app = express();
 app.use(morgan(`dev`));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
-connect();
 
-app.use("/", globalRouter);
+app.get("/" , (req, res) => {
+    res.render("screens/home");
+});
 
 app.listen(PORT, () => {
     console.log(`${PORT} SERVER START🥕`);
--- src/routers/globalRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import globalController from "../controllers/globalController";
-
-const globalRouter = express.Router();
-
-globalRouter.get("/", globalController.homeController);
-globalRouter.get("/post", globalController.postController);
-
-export default globalRouter;
