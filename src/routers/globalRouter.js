ba7a134537e3698b9a3c3e9991256212ac034f8c
Eun Jin <ouhouh2222@gmail.com>
2021-03-14 19:02:21 +0900

    signUp, forgotPassword

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -6,5 +6,7 @@ const globalRouter = express.Router();
 globalRouter.get("/", globalController.homeController);
 globalRouter.get("/login", globalController.loginController);
 globalRouter.get("/foodrecipe", globalController.foodrecipeController);
+globalRouter.get("/forgotPassword", globalController.forgotpasswordController);
+globalRouter.get("/signUp", globalController.signUpController);
 
 export default globalRouter;
