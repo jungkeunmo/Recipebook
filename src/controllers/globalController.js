cc9973884f4da441cb0ff70ba2dd08f9fb337acf
Eun Jin <ouhouh2222@gmail.com>
2021-03-14 18:59:25 +0900

    controller

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,22 +1,32 @@
 import dotenv from "dotenv";
+import { render } from "pug";
 dotenv.config();
 
 const homeController = (req, res) => {
-    res.render("screens/home");
+  res.render("screens/home");
 };
 
 const loginController = async (req, res) => {
-    res.render("screens/login");
+  res.render("screens/login");
 };
 
 const foodrecipeController = async (req, res) => {
-    res.render("screens/foodrecipe");
+  res.render("screens/foodrecipe");
 };
 
+const forgotpasswordController = async (req, res) => {
+  res.render("screens/forgotPassword");
+};
+
+const signUpController = async (req, res) => {
+  res.render("screens/signUP");
+};
 const globalController = {
-    homeController,
-    loginController,
-    foodrecipeController,
+  homeController,
+  loginController,
+  foodrecipeController,
+  forgotpasswordController,
+  signUpController,
 };
 
 export default globalController;
